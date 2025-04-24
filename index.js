f175dc9ac610e3018b6c4a8f6f037a55d0f59c99
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-23 21:13:47 -0300

    Ejercicio Culminado

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const createServer = require('./src/server');
 // Puerto en el que escuchará el servidor
 const PORT = 3000;
 
+const chalk = require('chalk')
+
 // Crear el servidor
 const server = createServer();
 
 // Iniciar el servidor
 server.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-  console.log('Presioná Ctrl+C para detener el servidor');
+  console.log(chalk.yellowBright(`Servidor escuchando en http://localhost:${PORT}`));
+  console.log(chalk.yellowBright('Presioná Ctrl+C para detener el servidor'));
 });
 
 // Exportar el servidor para pruebas
